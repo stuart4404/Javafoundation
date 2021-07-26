0727fe42ff93990fed0ea8085bfdf4b439170c1b
stuart4404 <stuart4404@gmail.com>
2021-07-26 12:14:31 +0100

    arrays in java

---
+++ java arrays.js
@@ -0,0 +1,25 @@
+
+
+//arrays
+
+var shoppinglist = ["jam","milk","eggs"];
+console.log(shoppinglist);
+
+// index number
+var eggs = shoppinglist[0]
+
+console.log(eggs);
+
+//change an item
+
+shoppinglist[1] = ("sausage");
+
+//remove an item
+
+shoppinglist.pop ("milk");
+
+//add an item
+
+shoppinglist.push("milko");
+
+console.log(shoppinglist)
